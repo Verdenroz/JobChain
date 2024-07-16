07e4376ceb7ba6bb90d80f8389b133206d62f2b2
harvey <harveytseng2@gmail.com>
2024-07-16 16:33:11 -0400

    Adapt MasterAgent and ReviewAgent to new changes

--- src/agents/memory/job_state.py
+++ src/agents/memory/job_state.py
@@ -3,8 +3,11 @@ from typing import List, Optional, TypedDict
 
 class JobAgentState(TypedDict):
     initial_query: str
+    max_results: int
     query: str
-    companies: Optional[List[str]]
-    field: Optional[str]
+    sources: Optional[List[str]]
     urls: Optional[List[str]]
+    job_listings: Optional[List[dict]]
     jobs: Optional[List[dict]]
+    max_revisions: int
+    revisions: int
--- src/agents/review.py
+++ src/agents/review.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 from langchain_community.adapters.openai import convert_openai_messages
 from langchain_community.document_loaders import AsyncHtmlLoader
@@ -7,25 +8,56 @@ from langchain_core.exceptions import OutputParserException
 from langchain_openai import ChatOpenAI
 
 from schemas.job import Job
+from src.agents.memory.job_state import JobAgentState
+from src.utils.model import invoke_model
 
 
 class ReviewAgent:
     def __init__(self):
         pass
 
-    async def revise(self, state):
+    async def revise(self, state: JobAgentState):
         """
         Checks if jobs should be revised if there are no jobs scraped
         :param state:
         :return: True if jobs should be revised
         """
-        jobs = state['jobs']
+        revisions = state.get('revisions') if state.get('revisions') else 1
+        if revisions >= state.get('max_revisions'):
+            return 'END'
+
+        jobs = state.get('jobs') if state.get('jobs') else []
         if not jobs or len(jobs) == 0:
             return True
 
         return False
 
-    async def review(self, state):
+    async def revise_query(self, state: JobAgentState):
+        """
+        Revises the query if there are no jobs scraped
+        :param state:
+        """
+        query = state.get('query')
+        revisions = state.get('revisions') if state.get('revisions') else 1
+        prompt = [{
+            "role": "system",
+            "content": "You are an expert in simplifying complicated queries.\n "
+        }, {
+            "role": "user",
+            "content": f"Given the query : {query}.\n"
+                       f"Your task is to simplify the query and make it even more concise\n"
+                       f"If there are multiple companies, you can simplify the query by focusing on one company\n"
+                       f"Please return  as a json with the field 'query' (str) and the simplified query as the value \n"
+        }]
+        response = invoke_model(prompt, 'gpt-3.5-turbo', response_format='json')
+        response_json = json.loads(response)
+
+        return {
+            "query": response_json.get('query'),
+            "revisions": revisions + 1,
+        }
+
+    async def review(self, state: JobAgentState):
         """
         Reviews jobs with more than 3 missing fields, updating them with new fields if possible
         :param state:
@@ -37,7 +69,8 @@ class ReviewAgent:
         # Filter Jobs
         for job in jobs:
             fields_to_check = ['job_description', 'job_location', 'job_posted_date', 'job_type', 'job_salary']
-            missing_fields = sum(1 for field in fields_to_check if not job.get(field))
+            missing_fields = sum(1 for field in fields_to_check if getattr(job, field, 'Unknown') in ['Unknown', None])
+
             if missing_fields > 3:
                 jobs_to_review.append(job)
 
@@ -59,16 +92,19 @@ class ReviewAgent:
         :return:
         """
         print(f"Reviewing job: {job}")
-        url = job['url']
+        url = job.url
         # Load the urls into Documents
         loader = AsyncHtmlLoader(url)
-        doc = loader.load()
+        try:
+            doc = loader.load()
+        except Exception as e:
+            print(f"Error loading document: {e}")
+            return None
 
-        # Transform the html content into text
+        # Try text transformer instead
         html2text = Html2TextTransformer()
         doc_transformed = html2text.transform_documents(doc)
 
-        # Shifts text downwards for potential new content
         text = doc_transformed[0].page_content[4000:6000]
         prompt = [{
             "role": "system",
