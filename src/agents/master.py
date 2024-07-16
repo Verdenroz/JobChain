d1e6922ed4827e378a0c31756b005c05d61b154a
harvey <harveytseng2@gmail.com>
2024-07-16 16:33:11 -0400

    Adapt MasterAgent and ReviewAgent to new changes

--- src/agents/master.py
+++ src/agents/master.py
@@ -1,9 +1,8 @@
-from typing import Optional, List
-
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 
-from src.agents.job_state import JobAgentState
+from src.agents.formatter import FormatterAgent
+from src.agents.memory.job_state import JobAgentState
 from src.agents.review import ReviewAgent
 from src.agents.scraper import ScraperAgent
 from src.agents.search import SearchAgent
@@ -12,7 +11,8 @@ from src.agents.semantics import SemanticsAgent
 
 class MasterAgent:
     def __init__(
-            self, query: str,
+            self,
+            query: str,
             sources=None,
             max_results: int = 5
     ):
@@ -22,15 +22,15 @@ class MasterAgent:
         self.sources = sources
         self.max_results = max_results
 
-    @staticmethod
-    def init_graph():
+    def init_graph(self):
         """
         Initialize the state graph with all nodes and edges
         :return: StateGraph(JobAgentState)
         """
         semantics_agent = SemanticsAgent()
-        search_agent = SearchAgent()
+        search_agent = SearchAgent(providers=self.sources)
         scraper_agent = ScraperAgent()
+        formatter_agent = FormatterAgent()
         review_agent = ReviewAgent()
 
         builder = StateGraph(JobAgentState)
@@ -38,7 +38,9 @@ class MasterAgent:
         builder.add_node('semantics', semantics_agent.clarify_query)
         builder.add_node('search', search_agent.find_job_urls)
         builder.add_node('scraper', scraper_agent.scrape_html)
+        builder.add_node('formatter', formatter_agent.format)
         builder.add_node('review', review_agent.review)
+        builder.add_node('revise', review_agent.revise_query)
 
         builder.add_conditional_edges(
             'semantics',
@@ -46,11 +48,14 @@ class MasterAgent:
             {False: END, True: 'search'}
         )
         builder.add_edge('search', 'scraper')
+
+        builder.add_edge('scraper', 'formatter')
         builder.add_conditional_edges(
-            'scraper',
+            'formatter',
             review_agent.revise,
-            {False: 'review', True: 'search'}
+            {False: 'review', True: 'revise', 'END': END}
         )
+        builder.add_edge('revise', 'search')
 
         builder.set_entry_point('semantics')
         builder.set_finish_point('review')
